c3f62abbcc1551faa53b6805af28aab5b7d003ce
Mina Atef <151832129+Mina-Atef-Marzouk-Gayed@users.noreply.github.com>
2023-12-01 20:44:00 +0200

    Update HashMap.h

--- .vscode/HashMap.h
+++ .vscode/HashMap.h
@@ -53,11 +53,15 @@ bool HashMap<KeyType, ValueType>::isEmpty() {
     return true;
 }
 template <typename KeyType, typename ValueType>
-void HashMap<KeyType, ValueType>::put( KeyType key, ValueType value) {
+void HashMap<KeyType, ValueType>::put(KeyType key, ValueType value) {
     int index = hashFunc(key);
-    map[index].key = key;
-    map[index].value = value;
-    
+    if (map[index].key == key) {
+        map[index].value = value;
+    }
+    else {
+        map[index].key = key;
+        map[index].value = value;
+    }
 }
 
 template <typename KeyType, typename ValueType>
