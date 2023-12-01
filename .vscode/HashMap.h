231f00636a4de65191297f2fc4fc214a5f355adf
MariamNasr <152440928+MariamN-x@users.noreply.github.com>
2023-12-01 16:27:59 +0200

    Update HashMap.h

--- .vscode/HashMap.h
+++ .vscode/HashMap.h
@@ -1,45 +1,60 @@
 #pragma once
-using namespace std;
+#include <iostream>
+
 template <typename Type>
+class HashMap {
+public:
+    class Entry {
+    public:
+        Type key;
+        Type value;
+        Entry(const Type& k, const Type& v) : key(k), value(v) {}
+    };
 
-
-class HashMap
-{
 private:
-const int capacity =20;
-  
-   Type map[capacity];
-   int hashFunc(Type key);
+    int capacity = 100;
+    Entry* map;
+    int hashFunc(Type key);
+
 public:
     HashMap();
     ~HashMap();
-    void put(Type key);
+    bool isEmpty();
+    void put(Type key, Type value);
     void remove(Type key);
     void display();
-    Type getVal(Type key)
-    bool isEmpty();
-
-
+    Type getVal(Type key);
 };
+
 template <typename Type>
-int HashMap<Type>:: hashFunc(Type key){
-int index = hash<Type>()(key);  //hashing function
+HashMap<Type>::HashMap() : map(new Entry[capacity]) {}
 
-
-return index %capacity;
-    
-}
 template <typename Type>
-bool HashMap<Type>::isEmpty(){
-int sum= 0;
-for (int i= 0; i<capacity ; i++){
-
-
-sum+= map[i];
-
+int HashMap<Type>::hashFunc(Type key) {
+    int index = 0;
+    for (int i = 0; i < key.length(); i++) {
+        index += static_cast<int>(key[i]);
+    }
+    return index % capacity;
 }
-if(!sum) return true;
 
-return false;
+template <typename Type>
+bool HashMap<Type>::isEmpty() {
+    for (int i = 0; i < capacity; i++) {
+        if (map[i].key.empty() && map[i].value != 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+template<typename Type>
+inline Type getVal(const Type& key) { 
+    int index = hashFunc(key);
+    return map[index].value;
+}
+
+template <typename Type>
+HashMap<Type>::~HashMap() {
+    delete[] map;
 }
