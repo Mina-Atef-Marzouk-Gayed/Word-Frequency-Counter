05d6739e8c591faeebaecc3fbd844eeb32a1b44f
Mina Atef <151832129+Mina-Atef-Marzouk-Gayed@users.noreply.github.com>
2023-12-02 23:44:37 +0200

    Update hashmap.cpp

--- .vscode/hashmap.cpp
+++ .vscode/hashmap.cpp
@@ -6,25 +6,7 @@ using namespace std;
 int main() {
     HashMap<std::string, int> map;
 
-    map.put("BMW");
-    map.put("LANCER");
-    map.put("KIA");
 
-    map.display();
-
-    // Get the value for a specific key
-    std::cout << "Value for 'banana': " << map.getVal("banana") << std::endl;
-
-    // Remove a key-value pair from the map
-    map.remove("apple");
-
-    // Check if the map is empty
-    if (map.isEmpty()) {
-        std::cout << "Map is empty" << std::endl;
-    }
-    else {
-        std::cout << "Map is not empty" << std::endl;
-    }
 
     HashMap<string, int> mapping;
     string text;
@@ -35,8 +17,8 @@ int main() {
     while (separate >> word) {
         mapping.put(word);
     }
-
     mapping.display();
+    cout<<mapping.getVal("LOL");
 
 
 
