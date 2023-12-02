b35386ce9a15c5c0a1e56a4ae9607721a7d818d1
MariamNasr <152440928+MariamN-x@users.noreply.github.com>
2023-12-03 00:03:48 +0200

    Hashmap.cpp

--- .vscode/hashmap.cpp
+++ .vscode/hashmap.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 #include "hashmap.h"
 using namespace std;
 
 int main() {
-    HashMap<std::string, int> map;
-
-
 
     HashMap<string, int> mapping;
     string text;
     cout << "Please input any text you would like : ";
     getline(cin, text);
+    replace(text.begin(), text.end(), '.', ' ');
     istringstream separate(text);
     string word;
     while (separate >> word) {
         mapping.put(word);
     }
     mapping.display();
-    cout<<mapping.getVal("LOL");
-
-
 
     return 0;
 }
