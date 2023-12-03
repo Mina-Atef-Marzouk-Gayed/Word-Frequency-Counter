80fbc065cb99b45bd460570d9214e2d1329813c9
MariamNasr <152440928+MariamN-x@users.noreply.github.com>
2023-12-03 11:46:35 +0200

    Hashmap.cpp

--- .vscode/hashmap.cpp
+++ .vscode/hashmap.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <sstream>
-#include <algorithm>
 #include "hashmap.h"
 using namespace std;
 
-int main() {
-
+void replaceNonCharactersWithSpace(std::string& text) {
+    for (char& c : text) {
+        if (!isalpha(c)) {
+            c = ' ';
+        }
+    }
+}
+void splitText(string& text)
+{
     HashMap<string, int> mapping;
-    string text;
-    cout << "Please input any text you would like : ";
-    getline(cin, text);
-    replace(text.begin(), text.end(), '.', ' ');
     istringstream separate(text);
     string word;
     while (separate >> word) {
         mapping.put(word);
     }
     mapping.display();
+    cout << "\n\ntest copy constructor and sorting" << endl;
+    HashMap<string, int> map2(mapping);
+    map2.display();
+
+}
+
+int main() {
+
+   
+    string text;
+    cout << "Please input any text you would like : ";
+    getline(cin, text);
+    replaceNonCharactersWithSpace(text);
+    splitText(text);
 
     return 0;
 }
+
