fcc29fef62135b3f6daade2fb6ca86ab9561324e
Mina Atef <151832129+Mina-Atef-Marzouk-Gayed@users.noreply.github.com>
2023-12-19 01:29:22 +0200

    Update HashMapProjectData.cpp

--- HashMapProjectData.cpp
+++ HashMapProjectData.cpp
@@ -97,26 +97,16 @@ void splitText(string& text)
 
 int main() {
 
-    string text = "We are a social media platform designed to help users find and share information about the latest news, events, and trends around the world. We believe in the power of people to share and collaborate on knowledge, and we are committed to fostering a community of curious learners who want to expand their understanding of the world around them. We offer a wide range of features and resources to help users stay up-to-date with the latest trends and developments in their areas of interest, from politics and business to science and culture.";
-   cout << "Please input any text you would like : ";
-   getline(cin, text); //We are a social media platform designed to help users find and share information about the latest news, events, and trends around the world.We believe in the power of people to share and collaborate on knowledge, and we are committed to fostering a community of curious learners who want to expand their understanding of the world around them.We offer a wide range of features and resources to help users stay up - to - date with the latest trends and developments in their areas of interest, from politics and business to science and culture.
-    replaceNonCharactersWithSpace(text);
-    splitText(text);
+    HashMap hashMap;
 
-    return 0;
+    cout << "Enter text \n";
+    string input;
+    getline(cin, input);
+
+    countSymbolsLettersDigitsWordsSentences(input);
+    replaceNonCharactersWithSpace(input);
+    splitText(input);
+
+
 }
 
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
